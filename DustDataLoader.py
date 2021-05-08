bc7340cf06446ab6995e26bb1e963c5e06c2688f
ylzf0000 <84978695@qq.com>
2021-05-08 20:37:51 +0800

    .

---
+++ DustDataLoader.py
@@ -0,0 +1,57 @@
+import os
+import glob
+import torch
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import cv2 as cv
+
+real_path = os.path.realpath(__file__)
+real_dir = real_path[:real_path.rfind('/')]
+data_dir = os.path.join(real_dir, 'data')
+print(data_dir)
+
+transform = transforms.Compose([
+    # transforms.ToPILImage(),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+
+class DustDataset(Dataset):
+    def __init__(self):
+        self.images = glob.glob(os.path.join(data_dir, '*.jpg'))
+        self.len = len(self.images)
+
+    def __getitem__(self, index):
+        img = self.images[index]
+        # x = cv.imread(img)
+        x = Image.open(img)
+        x = transform(x)
+        y = int(img[img.rfind('c') + 1:-4])
+        y = F.one_hot(torch.as_tensor([y]), num_classes=3)[0]
+        return x, y
+
+    def __len__(self):
+        return self.len
+
+
+# def my_collate(batch):
+#     data = [item[0] for item in batch]
+#     target = [item[1] for item in batch]
+#     return [data, target]
+
+dust_dataset = DustDataset()
+dust_dataloader = DataLoader(dust_dataset,
+                             batch_size=32,
+                             shuffle=True,
+                            #  collate_fn=my_collate
+                             )
+for X, Y in dust_dataloader:
+    print(X.shape, Y.shape)
+# if __name__ == '__main__':
+#     d = DustDataset()
